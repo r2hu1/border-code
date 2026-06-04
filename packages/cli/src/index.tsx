3decdd03a1550c7dd3eee3dde5c40894ae7baee5
r2hu1 <newasram86@gmail.com>
2026-06-05 00:42:28 +0530

    feat: integrate PromptInput and update app layout

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,19 +1,50 @@
-import { createCliRenderer } from "@opentui/core";
+import { createCliRenderer, TextAttributes } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 import { Header } from "./components/header";
 import { ThemeProvider } from "./providers/theme";
 import { ToastProvider } from "./providers/toast";
+import PromptInput from "./components/input/prompt-input";
+import { PromptConfigProvider } from "./providers/config/prompt-config";
 
 function App() {
 	return (
 		<ThemeProvider>
-			<ToastProvider>
-				<box alignItems="center" justifyContent="center" flexGrow={1}>
-					<box justifyContent="center" alignItems="flex-end">
+			<PromptConfigProvider>
+				<ToastProvider>
+					<box
+						alignItems="center"
+						justifyContent="center"
+						flexGrow={1}
+						gap={2}
+						position="relative"
+						width="100%"
+						height="100%"
+					>
 						<Header />
+						<box
+							width="100%"
+							maxWidth={78}
+							paddingX={2}
+							flexDirection="column"
+							gap={1}
+						>
+							<PromptInput />
+
+              <box flexDirection="row" gap={2} flexShrink={0} marginLeft="auto">
+                <box flexDirection="row" gap={1}>
+								<text>tab</text>
+								<text attributes={TextAttributes.DIM}>agents</text>
+                </box>
+                <box flexDirection="row" gap={1}>
+								<text>/</text>
+								<text attributes={TextAttributes.DIM}>command menu</text>
+                </box>
+							</box>
+
+						</box>
 					</box>
-				</box>
-			</ToastProvider>
+				</ToastProvider>
+			</PromptConfigProvider>
 		</ThemeProvider>
 	);
 }

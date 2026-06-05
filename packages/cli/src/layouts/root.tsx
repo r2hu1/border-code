605d611511cea18d1d9cfdc2f01cc7738febe429
r2hu1 <newasram86@gmail.com>
2026-06-05 21:48:22 +0530

    Fix RootLayout nesting indentation

--- packages/cli/src/layouts/root.tsx
+++ packages/cli/src/layouts/root.tsx
@@ -1,23 +1,28 @@
 import { Outlet } from "react-router";
 import { ToastProvider } from "../providers/toast";
-import { KeyboardLayerProvider, useKeyboardLayer } from "../providers/config/keyboard";
+import {
+	KeyboardLayerProvider,
+	useKeyboardLayer,
+} from "../providers/config/keyboard";
 import { ThemeProvider } from "../providers/theme";
 import { ThemedRoot } from "./themed-root";
-import { PromptConfigProvider, usePromptConfig } from "../providers/config/prompt-config";
+import {
+	PromptConfigProvider,
+	usePromptConfig,
+} from "../providers/config/prompt-config";
 import { DialogProvider } from "../providers/dialog";
 
 export function RootLayout() {
-
 	return (
 		<ThemeProvider>
 			<ToastProvider>
 				<KeyboardLayerProvider>
 					<DialogProvider>
-					<PromptConfigProvider>
-						<ThemedRoot>
-							<Outlet />
-						</ThemedRoot>
-					</PromptConfigProvider>
+						<PromptConfigProvider>
+							<ThemedRoot>
+								<Outlet />
+							</ThemedRoot>
+						</PromptConfigProvider>
 					</DialogProvider>
 				</KeyboardLayerProvider>
 			</ToastProvider>

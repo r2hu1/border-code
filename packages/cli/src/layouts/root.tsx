cc18037f0d9584b3eb6721333255db877aa28bf8
r2hu1 <newasram86@gmail.com>
2026-06-05 12:52:17 +0530

    chore: add missing exports to root layout imports

--- packages/cli/src/layouts/root.tsx
+++ packages/cli/src/layouts/root.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import { ToastProvider } from "../providers/toast";
-import { KeyboardLayerProvider } from "../providers/config/keyboard";
+import { KeyboardLayerProvider, useKeyboardLayer } from "../providers/config/keyboard";
 import { ThemeProvider } from "../providers/theme";
 import { ThemedRoot } from "./themed-root";
-import { PromptConfigProvider } from "../providers/config/prompt-config";
+import { PromptConfigProvider, usePromptConfig } from "../providers/config/prompt-config";
 import { DialogProvider } from "../providers/dialog";
 
 export function RootLayout() {
+
 	return (
 		<ThemeProvider>
 			<ToastProvider>

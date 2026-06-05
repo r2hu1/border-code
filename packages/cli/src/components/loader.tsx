c57652fac95b73dee26572d992f0428980bf839b
r2hu1 <newasram86@gmail.com>
2026-06-06 00:32:51 +0530

    Fix loader indentation

--- packages/cli/src/components/loader.tsx
+++ packages/cli/src/components/loader.tsx
@@ -3,12 +3,12 @@ import { Mode, type ModeType } from "@border-code/shared";
 import { useTheme } from "../providers/theme";
 
 type Props = {
-  mode?: ModeType;
+	mode?: ModeType;
 };
 
 export function Loader({ mode = Mode.BUILD }: Props) {
-  const { colors } = useTheme();
-  const activeColor = mode === Mode.PLAN ? colors.planMode : colors.primary;
+	const { colors } = useTheme();
+	const activeColor = mode === Mode.PLAN ? colors.planMode : colors.primary;
 
-  return <spinner name="aesthetic" color={activeColor} />;
-};
+	return <spinner name="aesthetic" color={activeColor} />;
+}

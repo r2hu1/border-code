38b4422ea0999efd9c3e7a72521f2e5aef5d8bd4
r2hu1 <newasram86@gmail.com>
2026-06-05 21:48:16 +0530

    Reformat ErrorMessage with consistent indentation

--- packages/cli/src/components/messages/error.tsx
+++ packages/cli/src/components/messages/error.tsx
@@ -3,34 +3,34 @@ import { EmptyBorder } from "../border";
 import { useTheme } from "../../providers/theme";
 
 type Props = {
-  message: string;
+	message: string;
 };
 
 export function ErrorMessage({ message }: Props) {
-  const { colors } = useTheme();
+	const { colors } = useTheme();
 
-  return (
-    <box width="100%" alignItems="center">
-      <box
-        border={["left"]}
-        borderColor={colors.error}
-        width="100%"
-        customBorderChars={{
-          ...EmptyBorder,
-          vertical: "┃",
-          bottomLeft: "╹",
-        }}
-      >
-        <box
-          justifyContent="center"
-          paddingX={2}
-          paddingY={1}
-          backgroundColor={colors.surface}
-          width="100%"
-        >
-          <text attributes={TextAttributes.DIM}>{message}</text>
-        </box>
-      </box>
-    </box>
-  );
-};
+	return (
+		<box width="100%" alignItems="center">
+			<box
+				border={["left"]}
+				borderColor={colors.error}
+				width="100%"
+				customBorderChars={{
+					...EmptyBorder,
+					vertical: "┃",
+					bottomLeft: "╹",
+				}}
+			>
+				<box
+					justifyContent="center"
+					paddingX={2}
+					paddingY={1}
+					backgroundColor={colors.surface}
+					width="100%"
+				>
+					<text attributes={TextAttributes.DIM}>{message}</text>
+				</box>
+			</box>
+		</box>
+	);
+}

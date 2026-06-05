cd1ecf7ef55d68142a9651482b7186be7870e736
r2hu1 <newasram86@gmail.com>
2026-06-05 21:48:19 +0530

    Reformat UserMessage with consistent indentation

--- packages/cli/src/components/messages/user.tsx
+++ packages/cli/src/components/messages/user.tsx
@@ -3,34 +3,35 @@ import { EmptyBorder } from "../border";
 import { useTheme } from "../../providers/theme";
 
 type Props = {
-  message: string;
-  mode: ModeType;
+	message: string;
+	mode: ModeType;
 };
 
 export function UserMessage({ message, mode }: Props) {
-  const { colors } = useTheme();
+	const { colors } = useTheme();
 
-  return (
-    <box width="100%" alignItems="center">
-      <box
-        border={["left"]}
-        borderColor={mode === Mode.PLAN ? colors.planMode : colors.primary}        width="100%"
-        customBorderChars={{
-          ...EmptyBorder,
-          vertical: "┃",
-          bottomLeft: "╹",
-        }}
-      >
-        <box
-          justifyContent="center"
-          paddingX={2}
-          paddingY={1}
-          backgroundColor={colors.surface}
-          width="100%"
-        >
-          <text>{message}</text>
-        </box>
-      </box>
-    </box>
-  );
-};
+	return (
+		<box width="100%" alignItems="center">
+			<box
+				border={["left"]}
+				borderColor={mode === Mode.PLAN ? colors.planMode : colors.primary}
+				width="100%"
+				customBorderChars={{
+					...EmptyBorder,
+					vertical: "┃",
+					bottomLeft: "╹",
+				}}
+			>
+				<box
+					justifyContent="center"
+					paddingX={2}
+					paddingY={1}
+					backgroundColor={colors.surface}
+					width="100%"
+				>
+					<text>{message}</text>
+				</box>
+			</box>
+		</box>
+	);
+}

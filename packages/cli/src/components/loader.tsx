738001e54008157238dcf3b80557febf1d6b6ab8
r2hu1 <newasram86@gmail.com>
2026-06-07 23:09:38 +0530

    format loader component

--- packages/cli/src/components/loader.tsx
+++ packages/cli/src/components/loader.tsx
@@ -3,11 +3,11 @@ import { Mode, type ModeType } from "@border-code/shared";
 import { useTheme } from "../providers/theme";
 
 type Props = {
-  mode?: ModeType;
+	mode?: ModeType;
 	name?: any;
 };
 
-export function Loader({ mode = Mode.BUILD,name="aesthetic" }: Props) {
+export function Loader({ mode = Mode.BUILD, name = "aesthetic" }: Props) {
 	const { colors } = useTheme();
 	const activeColor = mode === Mode.PLAN ? colors.planMode : colors.primary;
 
